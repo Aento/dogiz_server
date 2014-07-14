459e671486a3436689c45284dede3eed8cb25f78
mike-rozen <mike7416780@gmail.com>
2014-07-14 23:07:22 +0300

    ResolveConflict

--- src/java/doggizz/cloud/SendingMessage.java
+++ src/java/doggizz/cloud/SendingMessage.java
@@ -4,6 +4,7 @@
  */
 package doggizz.cloud;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import doggizz.classes.Gcm_id;
 import doggizz.sql.Gcm;
 import java.beans.PropertyVetoException;
@@ -17,7 +18,11 @@ import javax.security.sasl.SaslException;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.SmackException.NotConnectedException;
 import org.jivesoftware.smack.XMPPException;
-
+//IOS - JRE.
+import javapns.*;
+import javapns.devices.Device;
+import javapns.notification.*;
+import java.util.List;
 /**
  *
  * @author Stas
@@ -27,7 +32,7 @@ public class SendingMessage {
     {
         //IPHONE - 1
         //ANDROID - 2
-        
+        System.out.println("Sending Message Notification Entry");
         Gcm gcm = new Gcm();
         
         Gcm_id gcm_id = new Gcm_id();
@@ -38,9 +43,53 @@ public class SendingMessage {
         }
             
             
+        if(gcm_id.getOs_code() == 1){//IOS
+            try {
+            //Push.alert("Hello World!", "E:\\SQLServer\\SharedFolder\\PushNotificationCertificates\\Certificates.p12", "dogiz123", false,gcm_id.getGcm_id());
+                System.out.println("Ios Part");
+                List <PushedNotification> notifications = Push.badge(1, "E:\\SQLServer\\SharedFolder\\PushNotificationCertificates\\Certificates.p12", "dogiz123", false,gcm_id.getGcm_id());
+            
+                for (PushedNotification notification : notifications) {
+                                if (notification.isSuccessful()) {
+                                        /* Apple accepted the notification and should deliver it */  
+                                        System.out.println("Push notification sent successfully to: " +
+                                                                        notification.getDevice().getToken());
+                                          
+                                } else {
+                                        String invalidToken = notification.getDevice().getToken();
+                                        /* Add code here to remove invalidToken from your database */  
+                                       // List<Device> inactiveDevices = Push.feedback("keystore.p12", "dogiz123", false);
+                                        System.out.println("Invalid Token");
+                                        if(invalidToken != null && !invalidToken.isEmpty()){
+                                            System.out.println("Going to remove following token " +  notification.getDevice().getToken()+ "with user id =" + id);
+                                            
+                                        }
+                                        
+                                        /* Find out more about what the problem was */  
+                                        Exception theProblem = notification.getException();
+                                        theProblem.printStackTrace();
+        
+                                        /* If the problem was an error-response packet returned by Apple, get it */  
+                                        ResponsePacket theErrorResponse = notification.getResponse();
+                                        if (theErrorResponse != null) {
+                                                System.out.println(theErrorResponse.getMessage());
+                                        }
+                                }
+                }
+
+           
+           
+           } catch (Exception ex) { 
+               //Keystore or Communicatio Exceptoion may be thrown.
+                   Logger.getLogger(SendingMessage.class.getName()).log(Level.SEVERE, null, ex);
+            }
+           
+        }
+        
+            
             
         if(gcm_id.getOs_code()==2){
-            final String userName = "621192712917" + "@gcm.googleapis.com";
+         /*   final String userName = "621192712917" + "@gcm.googleapis.com";
             //final String userName = "621192712917@developer.gserviceaccount.com";
             final String password = "AIzaSyC6DPs9chpI8wQC-rqm887JxrYjVp6CCzg";
             SmackCcsClient ccsClient;
@@ -78,6 +127,19 @@ public class SendingMessage {
             } catch (PropertyVetoException ex) {
                 Logger.getLogger(SendingMessage.class.getName()).log(Level.SEVERE, null, ex);
             }
+            
+            
+*/
+            String apiKey = "AIzaSyC6DPs9chpI8wQC-rqm887JxrYjVp6CCzg";
+            Content c = new Content();
+            c.addRegId(gcm_id.getGcm_id());
+            c.createData("action","1");
+            try {
+                POST2GCM.post(apiKey, c);
+            } catch (JsonGenerationException ex) {
+                Logger.getLogger(SendingMessage.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
+    
 }
